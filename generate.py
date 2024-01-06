0bc18a4296a90b9e7e89033e6c06fb5ab3be34a9
interrrp <int@addictedto.coffee>
2024-01-07 07:27:00 +0800

    Generate one certificate for each shilling

--- generate.py
+++ generate.py
@@ -11,33 +11,21 @@ def main() -> None:
     template = TEMPLATE_PATH.read_text()
     CERTS_PATH.mkdir(exist_ok=True)
 
-    for name, shilling in entries.items():
-        create_certificate(name, shilling, template)
+    for name, shillings in entries.items():
+        if isinstance(shillings, str):
+            # Person only shills one thing
+            shillings = [shillings]
+
+        for shilling in shillings:
+            create_certificate(name, shilling, template)
 
 
-def create_certificate(name: str, shilling: str | list[str], template: str) -> None:
-    if isinstance(shilling, str):
-        # Person only shills one thing
-        shilling = [shilling]
-
-    cert_html = template.replace("{name}", name).replace(
-        "{shilling}", format_csv_with_and(shilling)
-    )
-    cert_path = CERTS_PATH / f"{name}.html"
+def create_certificate(name: str, shilling: str, template: str) -> None:
+    cert_html = template.replace("{name}", name).replace("{shilling}", shilling)
+    cert_path = CERTS_PATH / f"{name} ({shilling}).html"
     cert_path.write_text(cert_html)
 
     print(f"{name} -> {cert_path}")
-
-
-def format_csv_with_and(items: list[str]) -> str:
-    """Format a list of items as a comma-separated list with an "and" before the last item."""
-
-    if len(items) == 1:
-        return items[0]
-    elif len(items) == 2:
-        return f"{items[0]} and {items[1]}"
-    else:
-        return ", ".join(items[:-1]) + f", and {items[-1]}"
 
 
 if __name__ == "__main__":
